fc4494179b84c45ac5f774b1abc86fadab4d35c8
Simon Deeb <simon19-meet@meet.mit.edu>
2018-08-06 00:32:26 -0700

    database start

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -6,12 +6,11 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	__tablename__='students'
+	student_id=Column(Integer, primary_key=True)
+	wiki_name=Column(String)
+	topic=Column(String)
+	rating=Column(Integer)
+	
 
 	pass
